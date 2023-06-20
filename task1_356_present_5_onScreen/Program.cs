83461c6451270ab967b8819e6a0f62a7a276473e
zyich <zuilinho@yandex.ru>
2023-06-20 16:33:41 +0200

    task1

---
+++ task1_356_present_5_onScreen/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 10: Напишите программу, которая
+ принимает на вход трёхзначное число и на выходе 
+ показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+/*Console.WriteLine("input number");
+int num = Convert.ToInt32(Console.ReadLine());
+int preresult = num%100 - num%10;
+int result = preresult / 10 ;
+Console.WriteLine(result);
+*/
+
+int GetSecondNum (int num)
+{
+    
+ 
+    
+    int preresult = num%100 - num%10;
+    int result = preresult / 10 ;
+    return result;
+    
+
+}
+    Console.WriteLine("Input number");
+
+    int num = Convert.ToInt32(Console.ReadLine());
+    int result = GetSecondNum (num ) ;
+    Console.WriteLine(result);
